4e83fcd90d3adda54a99ebb72bd1bb015c6571ae
Sean Fagan <faganseanp@gmail.com>
2023-11-21 13:17:33 -0800

    form to register a user

--- Puddle/urls.py
+++ Puddle/urls.py
@@ -20,11 +20,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from core.views import index, contact
-
 urlpatterns = [
-    path("", index, name="index"),
+    path("", include("core.urls")),
     path("items/", include("item.urls")),
-    path("contact/", contact, name="contact"),
     path("admin/", admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# ^ media url addition for dev environment only, not recommended for production
